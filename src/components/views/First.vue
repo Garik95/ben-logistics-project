a747a17d7538b1e035576b64fb238f0dcd0d4113
Garik Nurmurodoff <garikn95@gmail.com>
2018-08-16 19:15:33 +0500

    last changes

--- src/components/views/First.vue
+++ src/components/views/First.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <md-dialog :md-active.sync="showDialog">
+    <md-dialog :md-active.sync="showDialog" @md-closed="dialogClose()" >
       <md-progress-bar class="md-accent" md-mode="indeterminate" v-if="dialogLoaded"></md-progress-bar>
       <md-dialog-title>Reserve Trailer: {{ tid }} - {{ tname }}</md-dialog-title>
       <md-card>
@@ -16,7 +16,7 @@
         <div class="md-layout md-gutter">
           <div class="md-layout-item">
             <md-content class="md-scrollbar">
-              <md-radio v-for="truck in trucks" :key="truck" v-model="truckRadio" :value="truck.label" v-on:change="onRadioSelect(truck.id)" >{{ truck.label }}</md-radio>
+              <md-radio v-for="truck in trucks" :key="truck" v-model="truckRadio" :value="truck.label" v-on:change="onRadioSelect(truck.id)" :disabled="!truck.isAvailable"  >{{ truck.label }}</md-radio>
             </md-content>
           </div>
           <div class="md-layout-item" v-if="showMap">
@@ -41,8 +41,8 @@
     </md-card>
 
       <md-dialog-actions>
-        <md-button class="md-primary" @click="dialogClose()">Close</md-button>
-        <md-button class="md-primary" @click="showDialog = false" v-if="!saveButton" >Reserve</md-button>
+        <md-button class="md-primary" @click="showDialog = false">Close</md-button>
+        <md-button class="md-primary" @click="addReserve()" v-if="!saveButton" >Reserve</md-button>
       </md-dialog-actions>
     </md-dialog>
     <md-progress-bar class="md-accent" md-mode="indeterminate" v-if="loaded"></md-progress-bar>
@@ -63,7 +63,7 @@
       </md-table-empty-state>
 
       <md-table-row slot="md-table-row" slot-scope="{ item }" md-selectable="single">
-        <md-table-cell md-label="Status" md-sort-by="status" md-numeric><md-avatar class="md-avatar-icon md-small" style="color:green">A</md-avatar></md-table-cell>
+        <md-table-cell md-label="Status" md-sort-by="status" md-numeric><md-badge v-bind:style="tStatus(item.status)" class="md-square" v-bind:md-content="item.status" /></md-table-cell>
         <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
         <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
         <md-table-cell md-label="Serial" md-sort-by="serial">{{ item.serial }}</md-table-cell>
@@ -75,6 +75,7 @@
         <md-table-cell md-label="Zip" md-sort-by="zip">{{ item.zip }}</md-table-cell>
       </md-table-row>
     </md-table>
+    <md-snackbar :md-active.sync="reserving">{{ msg }}</md-snackbar>
   </div>
 </template>
 
@@ -110,33 +111,55 @@ export default {
     saveButton: true,
     tid: '',
     tname: '',
-    trailerLoc: null
-    // selectedTrailer: null
+    truckid: '',
+    msg: '',
+    trailerLoc: null,
+    reserving: null
   }),
   methods: {
     searchOnTable () {
       this.searched = searchByName(this.trailers, this.search)
     },
     onSelect (item) {
-      this.showDialog = true
+      this.loaded = true
       this.tid = item.id
       this.tname = item.name
-      this.trailerLoc = {'lat': parseFloat(item.lat), 'lng': parseFloat(item.lng)}
+      axios.post('http://logistics-api.eu-4.evennode.com/graphql', {
+        query: `mutation{ addTempTrailer(id:` + this.tid + `, user:"` + this.$session.get('creds').login + `"){ id user } }`
+      }).then(response => {
+        if (response.data.data && !response.data.errors) {
+          this.trailerLoc = {'lat': parseFloat(item.lat), 'lng': parseFloat(item.lng)}
+          this.showDialog = true
+        } else {
+          this.msg = 'Choosen trailer is now reserving by other dispatcher'
+          this.reserving = true
+        }
+        this.loaded = false
+      })
     },
     dialogClose () {
+      axios.post('http://logistics-api.eu-4.evennode.com/graphql', {
+        query: `mutation{ deleteTempTrailer(id:` + this.tid + `){ id user } }`
+      })
       this.searchTruckInput = null
       this.showMap = false
       this.trucks = null
+      this.truckRadio = null
+      this.saveButton = true
       this.showDialog = false
     },
     searchTruck () {
       this.dialogLoaded = true
       axios.post('http://logistics-api.eu-4.evennode.com/graphql', {
-        query: `{ truck(label:` + this.searchTruckInput + `){ id label } }`
+        query: `{ truck(label:` + this.searchTruckInput + `){ id label isAvailable } }`
       }).then(response => {
-        this.trucks = response.data.data.truck
-        this.dialogLoaded = false
-        this.pos = null
+        if (this.showDialog === false) {
+          this.dialogLoaded = false
+        } else {
+          this.trucks = response.data.data.truck
+          this.dialogLoaded = false
+          this.pos = null
+        }
       })
     },
     onRadioSelect (id) {
@@ -144,17 +167,38 @@ export default {
       axios.post('http://logistics-api.eu-4.evennode.com/graphql', {
         query: `{ location(truckid:` + id + `){ lat:latitude lng:longitude } }`
       }).then(response => {
+        this.truckid = id
         this.pos = null
         this.pos = [{position: response.data.data.location[0]}, {position: this.trailerLoc}]
         this.showMap = true
         this.dialogLoaded = false
         this.saveButton = false
       })
+    },
+    addReserve () {
+      alert(this.tid)
+      alert(JSON.stringify(this.trailerLoc))
+      alert(this.truckid)
+      axios.post('http://logistics-api.eu-4.evennode.com/graphql', {
+        query: `mutation{ addReserve ( trailerid:` + this.tid + `, lat:"` + this.trailerLoc.lat + `", lng:"` + this.trailerLoc.lng + `", truckid:` + this.truckid + `, user:"` + this.$session.get('creds').login + `" ) { trailerid truckid time }}`
+      }).then(response => {
+        var res = response.data.data.addReserve
+        if (res.trailerid === this.tid && res.truckid === this.truckid && res.time) {
+          this.msg = 'Reserved!!!!'
+          this.reserving = true
+        }
+      })
+      this.showDialog = false
+    },
+    tStatus (st) {
+      if (st === 'hooked') return 'background-color:red'
+      if (st === 'reserved') return 'background-color:yellow; color:black'
+      if (st === 'available') return 'background-color:green'
     }
   },
   created () {
     axios.post('http://logistics-api.eu-4.evennode.com/graphql', {
-      query: `{ trailers{ address city state name serial id lat lng zip moving movingStartTime stopped stoppedStartTime } }`
+      query: `{ trailers{ address city state name serial id lat lng zip moving movingStartTime stopped stoppedStartTime status} }`
     }).then(response => {
       this.trailers = response.data.data.trailers
       this.searched = this.trailers
